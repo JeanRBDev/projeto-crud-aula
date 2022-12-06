5c76ee3fd52c3d15189765ff432fae3a19596f85
JeanRBDev <jbrodrigues158@gmail.com>
2022-12-06 14:16:59 -0300

    fix missing libs and dependencies

--- src/main/java/com/apirest/demo/Aluno.java
+++ src/main/java/com/apirest/demo/Aluno.java
@@ -1,5 +1,7 @@
 package com.apirest.demo;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/apirest/demo/RepositorioAlunos.java
+++ src/main/java/com/apirest/demo/RepositorioAlunos.java
@@ -2,6 +2,6 @@ package com.apirest.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RepositorioDados extends JpaRepository<Aluno, Long> {
+public interface RepositorioAlunos extends JpaRepository<Aluno, Long> {
 
 }
